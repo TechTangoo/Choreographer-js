130c83fe8831afcd7e7d78c20477ebeebd331220
priyankaj04 <priyankaj2002o4@gmail.com>
2023-11-17 09:27:04 +0530

    initial commit

--- script.js
+++ script.js
@@ -331,196 +331,98 @@
     }
 ]);
 
-var choreographer = new Choreographer({
-    animations: [
-        {
-            range: [1, window.innerHeight * 4],
-            selector: "#a",
-            type: "scale",
-            style: "rotate",
-            from: 0,
-            to: 360,
-            unit: "deg"
-        },
-        {
-            range: [1, window.innerHeight * 4],
-            selector: "#a",
-            type: "scale",
-            style: "scale",
-            from: 1,
-            to: 1.7,
-        },
-        {
-            range: [-1, window.innerHeight * 4],
-            selector: "#a",
-            type: "change",
-            style: "color",
-            from: "black",
-            to: "yellow"
-        },
-        {
-            range: [1, window.innerHeight * 4],
-            selector: "#b",
-            type: "scale",
-            style: "rotate",
-            from: 360,
-            to: 0,
-            unit: "deg"
-        },
-        {
-            range: [-1, window.innerHeight * 4],
-            selector: "#b",
-            type: "change",
-            style: "color",
-            from: "black",
-            to: "blue"
-        },
-        {
-            range: [-1, window.innerHeight * 4],
-            selector: "#b",
-            type: "scale",
-            style: "opacity",
-            from: 1,
-            to: 0.3
-        },
-        {
-            range: [1, window.innerHeight * 4],
-            selector: "#c",
-            type: "rotate",
-            style: "transform",
-            from: "rotateY(0deg)",
-            to: "rotateY(360deg)",
-        },
-        {
-            range: [-1, window.innerHeight * 4],
-            selector: "#c",
-            type: "change",
-            style: "color",
-            from: "black",
-            to: "yellow"
-        },
-        {
-            range: [1, window.innerHeight * 4],
-            selector: "#d",
-            type: "scale",
-            style: "rotate",
-            from: 0,
-            to: 360,
-            unit: "deg"
-        },
-        {
-            range: [-1, window.innerHeight * 4],
-            selector: "#d",
-            type: "change",
-            style: "color",
-            from: "black",
-            to: "yellow"
-        },
-        {
-            range: [1, window.innerHeight * 4],
-            selector: "#e",
-            type: "scale",
-            style: "rotate",
-            from: 0,
-            to: 360,
-            unit: "deg"
-        },
-        {
-            range: [-1, window.innerHeight * 4],
-            selector: "#e",
-            type: "change",
-            style: "color",
-            from: "black",
-            to: "yellow"
-        },
-        {
-            range: [1, window.innerHeight * 4],
-            selector: "#f",
-            type: "scale",
-            style: "rotate",
-            from: 0,
-            to: 360,
-            unit: "deg"
-        },
-        {
-            range: [-1, window.innerHeight * 4],
-            selector: "#f",
-            type: "change",
-            style: "color",
-            from: "black",
-            to: "yellow"
-        },
-        {
-            range: [1, window.innerHeight * 4],
-            selector: "#g",
-            type: "scale",
-            style: "rotate",
-            from: 0,
-            to: 360,
-            unit: "deg"
-        },
-        {
-            range: [-1, window.innerHeight * 4],
-            selector: "#g",
-            type: "change",
-            style: "color",
-            from: "black",
-            to: "yellow"
-        },
-        {
-            range: [1, window.innerHeight * 4],
-            selector: "#h",
-            type: "scale",
-            style: "rotate",
-            from: 0,
-            to: 360,
-            unit: "deg"
-        },
-        {
-            range: [-1, window.innerHeight * 4],
-            selector: "#h",
-            type: "change",
-            style: "color",
-            from: "black",
-            to: "yellow"
-        },
-        {
-            range: [1, window.innerHeight * 4],
-            selector: "#i",
-            type: "scale",
-            style: "rotate",
-            from: 0,
-            to: 360,
-            unit: "deg"
-        },
-        {
-            range: [-1, window.innerHeight * 4],
-            selector: "#i",
-            type: "change",
-            style: "color",
-            from: "black",
-            to: "yellow"
-        },
-        {
-            range: [1, window.innerHeight * 4],
-            selector: "#j",
-            type: "scale",
-            style: "rotate",
-            from: 0,
-            to: 360,
-            unit: "deg"
-        },
-        {
-            range: [-1, window.innerHeight * 4],
-            selector: "#j",
-            type: "change",
-            style: "color",
-            from: "black",
-            to: "yellow"
-        },
-    ]
-});
+var wrapperNode = document.getElementById('wrapper')
+var scrollDownNode = document.querySelector('.scroll-down')
+var linkNodes = document.querySelector('.links')
 
-window.addEventListener("scroll", function () {
-    choreographer.runAnimationsAt(window.scrollY);
-});
+var vh = window.innerHeight
+
+/* STORE SOME KEY LOCATIONS */
+
+/* ~ le fin ~
+ * The point where you cannot scroll down any further.
+ */
+var fin = wrapperNode.clientHeight - vh + linkNodes.clientHeight
+
+function calculateAnimations() {
+    return [
+        /* animate Cs */
+        { range: [-1, fin * 0.5], selector: '.c', type: 'scale', style: 'transform:translateY', from: 0, to: 25, unit: 'px' },
+        { range: [fin * 0.5, fin], selector: '.c', type: 'scale', style: 'transform:translateY', from: 25, to: 0, unit: 'px' },
+        { range: [fin * 0.4, fin], selector: '.c', type: 'change', style: 'color', to: '#ffb515' },
+
+        /* animate Hs */
+        { range: [-1, fin * 0.5], selector: '.h', type: 'scale', style: 'transform:scaleX', from: 1, to: 0.5 },
+        { range: [-1, fin * 0.5], selector: '.h', type: 'scale', style: 'transform:scaleY', from: 1, to: 0.5 },
+        { range: [fin * 0.5, fin], selector: '.h', type: 'scale', style: 'transform:scaleX', from: 0.5, to: 1 },
+        { range: [fin * 0.5, fin], selector: '.h', type: 'scale', style: 'transform:scaleY', from: 0.5, to: 1 },
+        { range: [fin * 0.3, fin], selector: '.h', type: 'change', style: 'color', to: '#1fd1ec' },
+
+        /* animate Os */
+        { range: [fin * 0.1, fin], selector: '.o', type: 'randomizeColor' },
+
+        /* animate Rs */
+        { range: [-1, fin * 0.5], selector: '.r', type: 'scale', style: 'transform:rotateX', from: 0, to: 90, unit: 'deg' },
+        { range: [fin * 0.5, fin], selector: '.r', type: 'scale', style: 'transform:rotateX', from: 90, to: 0, unit: 'deg' },
+        { range: [fin * 0.3, fin], selector: '.r', type: 'change', style: 'color', to: '#8382f9' },
+
+        /* animate Es */
+        { range: [fin * 0.3, fin], selector: '.e', type: 'change', style: 'color', to: '#ff1b9b' },
+
+        /* animate Gs */
+        { range: [-1, fin * 0.5], selectors: ['.g', '.j'], type: 'scale', style: 'transform:rotateZ', from: 0, to: 180, unit: 'deg' },
+        { range: [fin * 0.5, fin], selectors: ['.g', '.j'], type: 'scale', style: 'transform:rotateZ', from: 180, to: 360, unit: 'deg' },
+        { range: [fin * 0.4, fin], selectors: ['.g', '.j'], type: 'change', style: 'color', to: '#ff8b1c' },
+
+        /* animate As */
+        { range: [-1, fin * 0.5], selectors: ['.a', '.s'], type: 'scale', style: 'transform:rotateZ', from: 0, to: -180, unit: 'deg' },
+        { range: [fin * 0.5, fin], selectors: ['.a', '.s'], type: 'scale', style: 'transform:rotateZ', from: -180, to: -360, unit: 'deg' },
+        { range: [fin * 0.4, fin], selectors: ['.a', '.s'], type: 'change', style: 'color', to: '#c05bdb' },
+
+        /* animate Ps */
+        { range: [-1, fin * 0.5], selectors: ['.p', '.dash'], type: 'scale', style: 'opacity', from: 1, to: 0.1 },
+        { range: [fin * 0.5, fin], selectors: ['.p', '.dash'], type: 'scale', style: 'opacity', from: 0.1, to: 1 },
+        { range: [fin * 0.4, fin], selectors: ['.p', '.dash'], type: 'change', style: 'color', to: '#ff537c' },
+
+        /* animate line */
+        { range: [-1, fin], selector: '.line', type: 'scale', style: 'width', from: 0.01, to: 50, unit: '%' },
+        { range: [-1, fin], selector: '.line', type: 'scale', style: 'opacity', from: 0, to: 1 },
+
+        /* animate arrow */
+        { range: [0.6 * fin, fin], selector: '.scroll-down', type: 'scale', style: 'opacity', from: 1, to: 0 },
+        { range: [fin - 30, fin], selector: '.scroll-down', type: 'change', style: 'display', to: 'none' },
+
+        /* animate links */
+        { range: [0.8 * fin, fin], selector: '.links', type: 'scale', style: 'opacity', from: 0, to: 1 }
+    ]
+}
+
+// Instantiate choreographer.
+var choreographer = new Choreographer({
+    animations: calculateAnimations(),
+    customFunctions: {
+        randomizeColor: function (data) {
+            var chars = '0123456789abcdef'.split('')
+            var hex = '#'
+
+            while (hex.length < 7) {
+                hex += chars[Math.round(Math.random() * (chars.length - 1))]
+            }
+
+            data.node.style.color = hex
+        }
+    }
+})
+
+function animate() {
+    var scrollPosition = (wrapperNode.getBoundingClientRect().top - wrapperNode.offsetTop) * -1
+    choreographer.runAnimationsAt(scrollPosition)
+}
+
+document.body.addEventListener('scroll', animate)
+
+animate()
+
+window.addEventListener('resize', function () {
+    choreographer.updateAnimations(calculateAnimations())
+})
